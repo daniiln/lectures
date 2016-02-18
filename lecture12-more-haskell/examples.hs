79a27776abfc97b8d538dcec344822d0e8ed32b6
Daniil Nguen <dn393@drexel.edu>
2016-02-18 14:52:35 -0500

    lecture 12

--- lecture12-more-haskell/examples.hs
+++ lecture12-more-haskell/examples.hs
@@ -1,17 +1,34 @@
+import Prelude hiding (map, appned, take, drop)
+
 data MaybeHead = NoHead
                | JustHead Int
 
--- safeHeadInts ::
+{-data Maybe a = Nothing-}
+             {-| Just a-}
 
+safeHeadInts :: [Int] -> MaybeHead
+safeHeadInts [] = NoHead
+safeHeadInts (x:_) = JustHead x
+
+safeHead :: [a] -> Mayber a
+safeHead [] = Nothing
+safeHead {x:_} = Just x
 --
 -- List exercises
 --
 
--- map ::
+map :: (a -> b) -> [a] -> [b]
+map _ [] = []
+map f (x:xs) = f x : map f xs
 
--- append ::
+append :: [a] -> [a] -> [a]
+append []    ys = ys
+append (x:xs) ys = x:append xs ys
 
--- take ::
+take :: Int -> [a] -> [a]
+take 0 _    = []
+take _ []   = []
+take n (x:xs) - x : take (n-1) xs
 
 -- drop ::
 
@@ -19,32 +36,46 @@ data MaybeHead = NoHead
 -- define a List data type
 --
 
--- data List
+data List a = Nil 
+            | Cons a (List a)
 
 --
 -- define a NonEmptyList data type and associated functions
 --
 
--- data NonEmptyList
+data NonEmptyList a = NEL a [a] 
 
--- nelToList ::
+nelToList :: NonEmptyList a -> [a]
+nelToList (NEL x xs) = x : xs
 
--- listToNel ::
+listToNel :: [a] -> NonEmptyList a
+listToNel [] = Nothing
+listToNel (x:xs) = Just (NEL x xs)
+{-listToNel (x:xs) = Just $ NEL x xs-}
 
--- headNEL ::
+headNEL :: NonEmptyList a -> a 
+headNEL (NEL x _) = x
 
--- tailNEL ::
+tailNEL :: NonEmptyList a -> [a]
 
 --
 -- Higher-order functions
 --
 
--- (.) :: (b -> c) -> (a -> b) -> a -> c
+(.) :: (b -> c) -> (a -> b) -> a -> c
+--(.) f g x = f (g x) 
+f . g = \ x -> f (g x)
 
--- ($) :: (a -> b) -> a -> b
+($) :: (a -> b) -> a -> b
+f $ x = f x
 
 -- What about papply?
 
 data Foo = F Int | G Char
+
+instance Eq Foo where
+    F x == F y = x == y
+    G x == G y = x == y
+    ?????????????????????????
 
 -- instance Eq Foo where
